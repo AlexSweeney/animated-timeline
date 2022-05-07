7d200b8f444927781eeb6047a98c936ab524b9d0
Alex Sweeney <a.sweeney@ryte.com>
2022-05-07 19:08:31 +0100

    Timeline - fix - make line same size as TimelineSections

--- src/components/Timeline/parts/Timeline.tsx
+++ src/components/Timeline/parts/Timeline.tsx
@@ -82,147 +82,149 @@ const Timeline = () => {
       <div className="timeline__container" id={timelineId}>   
         <h3 className="timeline__title">Our Journey</h3>
 
-        <div className='timeline__content-wrapper'>
-          <div className='timeline__line-wrapper'>
-            <div id={lineId} className={`timeline__line ${lineOpenClass}`}/>
-          </div>
-          <div className='timeline__sections-wrapper'> 
-          <TimelineSection 
-              addId={addId} 
-              isAnimating={isAnimating} 
-              animateId={animateId} 
-              nextAnimation={nextAnimation} 
-            >   
-              <h4>2021</h4>
-              <p>New Knowledge Hub platform launches</p>
-            </TimelineSection>
+        <div className='timeline__content-container'>
+          <div className='timeline__content-wrapper'>
+            <div className='timeline__sections-wrapper'> 
+                <TimelineSection 
+                  addId={addId} 
+                  isAnimating={isAnimating} 
+                  animateId={animateId} 
+                  nextAnimation={nextAnimation} 
+                >   
+                  <h4>2021</h4>
+                  <p>New Knowledge Hub platform launches</p>
+                </TimelineSection>
 
-            <TimelineSection 
-              addId={addId} 
-              isAnimating={isAnimating} 
-              animateId={animateId} 
-              nextAnimation={nextAnimation} 
-              infoBoxPosition={InfoBoxPosition.Right}
-            >
-              <h4>2020</h4>
-              <p>Million parts sold globally - Audio Audio launched</p>
-            </TimelineSection>    
+                <TimelineSection 
+                  addId={addId} 
+                  isAnimating={isAnimating} 
+                  animateId={animateId} 
+                  nextAnimation={nextAnimation} 
+                  infoBoxPosition={InfoBoxPosition.Right}
+                >
+                  <h4>2020</h4>
+                  <p>Million parts sold globally - Audio Audio launched</p>
+                </TimelineSection>    
 
-            <TimelineSection 
-              addId={addId} 
-              isAnimating={isAnimating} 
-              animateId={animateId} 
-              nextAnimation={nextAnimation} 
-            >
-              <h4>2019</h4>  
-              <p>German Sales office is opened forming global HQ</p>
-            </TimelineSection> 
+                <TimelineSection 
+                  addId={addId} 
+                  isAnimating={isAnimating} 
+                  animateId={animateId} 
+                  nextAnimation={nextAnimation} 
+                >
+                  <h4>2019</h4>  
+                  <p>German Sales office is opened forming global HQ</p>
+                </TimelineSection> 
 
-            <TimelineSection 
-              addId={addId} 
-              isAnimating={isAnimating} 
-              animateId={animateId} 
-              nextAnimation={nextAnimation} 
-              infoBoxPosition={InfoBoxPosition.Right}
-            >
-              <h4>2018</h4>  
-              <p>German distribution center in opened</p>
-            </TimelineSection>  
+                <TimelineSection 
+                  addId={addId} 
+                  isAnimating={isAnimating} 
+                  animateId={animateId} 
+                  nextAnimation={nextAnimation} 
+                  infoBoxPosition={InfoBoxPosition.Right}
+                >
+                  <h4>2018</h4>  
+                  <p>German distribution center in opened</p>
+                </TimelineSection>  
 
-            <TimelineSection 
-              addId={addId} 
-              isAnimating={isAnimating} 
-              animateId={animateId} 
-              nextAnimation={nextAnimation}  
-            >
-              <h4>2017</h4>  
-              <p>First Office in USA opens - Second UK office opens</p>   
-            </TimelineSection>  
+                <TimelineSection 
+                  addId={addId} 
+                  isAnimating={isAnimating} 
+                  animateId={animateId} 
+                  nextAnimation={nextAnimation}  
+                >
+                  <h4>2017</h4>  
+                  <p>First Office in USA opens - Second UK office opens</p>   
+                </TimelineSection>  
 
-            <TimelineSection 
-              addId={addId} 
-              isAnimating={isAnimating} 
-              animateId={animateId} 
-              nextAnimation={nextAnimation} 
-              infoBoxPosition={InfoBoxPosition.Right}
-            >
-              <h4>2016</h4>  
-              <p>EU Automation INC is formed - Singapore Office and warehouse opens</p>   
-            </TimelineSection> 
+                <TimelineSection 
+                  addId={addId} 
+                  isAnimating={isAnimating} 
+                  animateId={animateId} 
+                  nextAnimation={nextAnimation} 
+                  infoBoxPosition={InfoBoxPosition.Right}
+                >
+                  <h4>2016</h4>  
+                  <p>EU Automation INC is formed - Singapore Office and warehouse opens</p>   
+                </TimelineSection> 
 
-            <TimelineSection 
-              addId={addId} 
-              isAnimating={isAnimating} 
-              animateId={animateId} 
-              nextAnimation={nextAnimation}  
-            >
-              <h4>2015</h4>   
-              <p>First customer to reach 1000 orders</p> 
-            </TimelineSection> 
+                <TimelineSection 
+                  addId={addId} 
+                  isAnimating={isAnimating} 
+                  animateId={animateId} 
+                  nextAnimation={nextAnimation}  
+                >
+                  <h4>2015</h4>   
+                  <p>First customer to reach 1000 orders</p> 
+                </TimelineSection> 
 
-            <TimelineSection 
-              addId={addId} 
-              isAnimating={isAnimating} 
-              animateId={animateId} 
-              nextAnimation={nextAnimation} 
-              infoBoxPosition={InfoBoxPosition.Right}
-            >
-              <h4>2014</h4> 
-              <p>AUTOMATED magazine launches</p>     
-            </TimelineSection> 
+                <TimelineSection 
+                  addId={addId} 
+                  isAnimating={isAnimating} 
+                  animateId={animateId} 
+                  nextAnimation={nextAnimation} 
+                  infoBoxPosition={InfoBoxPosition.Right}
+                >
+                  <h4>2014</h4> 
+                  <p>AUTOMATED magazine launches</p>     
+                </TimelineSection> 
 
-            <TimelineSection 
-              addId={addId} 
-              isAnimating={isAnimating} 
-              animateId={animateId} 
-              nextAnimation={nextAnimation}  
-            >
-              <h4>2013</h4>   
-              <p>Delivery made to 100th country</p>    
-            </TimelineSection> 
+                <TimelineSection 
+                  addId={addId} 
+                  isAnimating={isAnimating} 
+                  animateId={animateId} 
+                  nextAnimation={nextAnimation}  
+                >
+                  <h4>2013</h4>   
+                  <p>Delivery made to 100th country</p>    
+                </TimelineSection> 
 
-            <TimelineSection 
-              addId={addId} 
-              isAnimating={isAnimating} 
-              animateId={animateId} 
-              nextAnimation={nextAnimation} 
-              infoBoxPosition={InfoBoxPosition.Right}
-            >
-              <h4>2012</h4>    
-              <p>UK distribution center opens</p>  
-            </TimelineSection> 
+                <TimelineSection 
+                  addId={addId} 
+                  isAnimating={isAnimating} 
+                  animateId={animateId} 
+                  nextAnimation={nextAnimation} 
+                  infoBoxPosition={InfoBoxPosition.Right}
+                >
+                  <h4>2012</h4>    
+                  <p>UK distribution center opens</p>  
+                </TimelineSection> 
 
-            <TimelineSection 
-              addId={addId} 
-              isAnimating={isAnimating} 
-              animateId={animateId} 
-              nextAnimation={nextAnimation}  
-            >
-              <h4>2011</h4>       
-              <p>First customer reaches 100 orders</p>
-            </TimelineSection> 
+                <TimelineSection 
+                  addId={addId} 
+                  isAnimating={isAnimating} 
+                  animateId={animateId} 
+                  nextAnimation={nextAnimation}  
+                >
+                  <h4>2011</h4>       
+                  <p>First customer reaches 100 orders</p>
+                </TimelineSection> 
 
-            <TimelineSection 
-              addId={addId} 
-              isAnimating={isAnimating} 
-              animateId={animateId} 
-              nextAnimation={nextAnimation} 
-              infoBoxPosition={InfoBoxPosition.Right}
-            >
-              <h4>2010</h4>      
-              <p>EU Automations first UK office</p>
-            </TimelineSection> 
+                <TimelineSection 
+                  addId={addId} 
+                  isAnimating={isAnimating} 
+                  animateId={animateId} 
+                  nextAnimation={nextAnimation} 
+                  infoBoxPosition={InfoBoxPosition.Right}
+                >
+                  <h4>2010</h4>      
+                  <p>EU Automations first UK office</p>
+                </TimelineSection> 
 
-            <TimelineSection 
-              addId={addId} 
-              isAnimating={isAnimating} 
-              animateId={animateId} 
-              nextAnimation={nextAnimation}  
-              className="timeline-section--last"
-            >
-              <h4>2009</h4>  
-              <p>EU Automation is formed in Stafforshire, UK</p>     
-            </TimelineSection>
+                <TimelineSection 
+                  addId={addId} 
+                  isAnimating={isAnimating} 
+                  animateId={animateId} 
+                  nextAnimation={nextAnimation}  
+                  className="timeline-section--last"
+                >
+                  <h4>2009</h4>  
+                  <p>EU Automation is formed in Stafforshire, UK</p>     
+                </TimelineSection>
+            </div>
+            <div className='timeline__line-wrapper'>
+              <div id={lineId} className={`timeline__line ${lineOpenClass}`}/>
+            </div> 
           </div>
         </div>
         {/* <div className='timeline__inner'>
