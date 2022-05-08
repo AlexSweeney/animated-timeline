3ba5552dc05caf81704e07c52488b1cef4c6aed2
Alex Sweeney <a.sweeney@ryte.com>
2022-05-08 14:14:42 +0100

    Timeline - refactor - use enum for classes

--- src/components/Timeline/parts/Timeline.tsx
+++ src/components/Timeline/parts/Timeline.tsx
@@ -3,7 +3,7 @@ import TimelineSection from './TimelineSection';
 import { triggerOnTransitionEnd } from '../../../utils/utils';
 import './../styles/Timeline.scss';
 import { v4 as uuid } from 'uuid';
-import { InfoBoxPosition } from './../utils/TimelineSection.consts';
+import { InfoBoxPosition, LineOpenClass } from './../utils/TimelineSection.consts';
 
 export const TimelineContext: any = createContext(null);
 
@@ -18,7 +18,7 @@ const Timeline = () => {
   const [isAnimating, setIsAnimating] = useState<boolean>(false);
   const [animateId, setAnimateId] = useState<string>(''); 
   // === classes
-  const [lineOpenClass, setLineOpenClass] = useState<string>('timeline__line--closed');
+  const [lineOpenClass, setLineOpenClass] = useState<string>(LineOpenClass.Closed);
 
   // === event handlers
   const onRender = () => { 
@@ -46,7 +46,7 @@ const Timeline = () => {
       triggerOnTransitionEnd(lineId, 'height', () => { 
         resolve(null)
       }) 
-      setLineOpenClass('timeline__line--open')
+      setLineOpenClass(LineOpenClass.Open)
     }) 
   };
 
@@ -84,7 +84,7 @@ const Timeline = () => {
       <section className="timeline__container" id={timelineId}>   
         <h3 className="timeline__title">Our Journey</h3>
 
-        <div className='timeline__content-container'>
+        <div className='timeline__content-wrapper'>
           <TimelineSection>   
             <h4>2021</h4>
             <p>New Knowledge Hub platform launches</p>
@@ -158,85 +158,6 @@ const Timeline = () => {
             <div id={lineId} className={`timeline__line ${lineOpenClass}`}/>
           </div> 
         </div>
-
-        
-        {/* <div className='timeline__content-container'>
-          <div className='timeline__content-wrapper'>
-            <div className='timeline__sections-wrapper'>   
-              <TimelineSection>   
-                <h4>2021</h4>
-                <p>New Knowledge Hub platform launches</p>
-              </TimelineSection>
-
-              <TimelineSection infoBoxPosition={InfoBoxPosition.Right}>
-                <h4>2020</h4>
-                <p>Million parts sold globally - Audio Audio launched</p>
-              </TimelineSection>    
-
-              <TimelineSection>
-                <h4>2019</h4>  
-                <p>German Sales office is opened forming global HQ</p>
-              </TimelineSection> 
-
-              <TimelineSection infoBoxPosition={InfoBoxPosition.Right}>
-                <h4>2018</h4>  
-                <p>German distribution center in opened</p>
-              </TimelineSection>  
-
-              <TimelineSection>
-                <h4>2017</h4>  
-                <p>First Office in USA opens - Second UK office opens</p>   
-              </TimelineSection>  
-
-              <TimelineSection infoBoxPosition={InfoBoxPosition.Right}>
-                <h4>2016</h4>  
-                <p>EU Automation INC is formed - Singapore Office and warehouse opens</p>   
-              </TimelineSection> 
-
-              <TimelineSection>
-                <h4>2015</h4>   
-                <p>First customer to reach 1000 orders</p> 
-              </TimelineSection> 
-
-              <TimelineSection infoBoxPosition={InfoBoxPosition.Right}>
-                <h4>2014</h4> 
-                <p>AUTOMATED magazine launches</p>     
-              </TimelineSection> 
-
-              <TimelineSection>
-                <h4>2013</h4>   
-                <p>Delivery made to 100th country</p>    
-              </TimelineSection> 
-
-              <TimelineSection infoBoxPosition={InfoBoxPosition.Right}>
-                <h4>2012</h4>    
-                <p>UK distribution center opens</p>  
-              </TimelineSection> 
-
-              <TimelineSection>
-                <h4>2011</h4>       
-                <p>First customer reaches 100 orders</p>
-              </TimelineSection> 
-
-              <TimelineSection  
-                infoBoxPosition={InfoBoxPosition.Right}
-              >
-                <h4>2010</h4>      
-                <p>EU Automations first UK office</p>
-              </TimelineSection> 
-
-              <TimelineSection  
-                className="timeline-section--last"
-              >
-                <h4>2009</h4>  
-                <p>EU Automation is formed in Stafforshire, UK</p>     
-              </TimelineSection> 
-            </div>
-            <div className='timeline__line-wrapper'>
-              <div id={lineId} className={`timeline__line ${lineOpenClass}`}/>
-            </div> 
-          </div>
-        </div>  */}
       </section> 
     </TimelineContext.Provider>
   )
--- src/components/Timeline/utils/TimelineSection.consts.ts
+++ src/components/Timeline/utils/TimelineSection.consts.ts
@@ -2,3 +2,8 @@ export enum InfoBoxPosition {
   Left = 'left',
   Right = 'right',
 }
+
+export enum LineOpenClass {
+  Open = 'timeline__line--open',
+  Closed = 'timeline__line--closed',
+}
